05449063a4ef09aef6518c35a86fa275e180065c
Nanashi <alksndr.zln@gmail.com>
2023-02-10 21:14:41 +0300

    Added Image model for handling images

--- places/models.py
+++ places/models.py
@@ -4,10 +4,19 @@ from django.db import models
 class Place(models.Model):
 
     title = models.CharField(max_length=256)
-    img_urls = models.TextField(null=True)
     description_short = models.TextField()
     description_long = models.TextField()
     coordinates = models.CharField(max_length=256)
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+
+    title = models.CharField(max_length=256)
+    order = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.order} {self.title}'
