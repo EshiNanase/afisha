82a8bdefbd5f95af49bf116cfffbb9b5e972e984
Nanashi <alksndr.zln@gmail.com>
2023-02-10 21:55:21 +0300

    Added media files serving

--- places/models.py
+++ places/models.py
@@ -7,6 +7,8 @@ class Place(models.Model):
     description_short = models.TextField()
     description_long = models.TextField()
     coordinates = models.CharField(max_length=256)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
 
     def __str__(self):
         return self.title
--- places/views.py
+++ places/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from places.models import Place
 
 
 class MapView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MapView, self).get_context_data(**kwargs)
+        context['places'] = Place.objects.all()
+        print(context['places'].first().coordinates)
+        return context
