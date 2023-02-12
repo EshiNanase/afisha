99fff138c0fdd9709568bf3ca9beab91fc57a0e4
Aleksandr Piksasov <70457542+EshiNanase@users.noreply.github.com>
2023-02-12 12:11:23 +0300

    Add files via upload

---
+++ places/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from places.models import Place, Image
+from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
+
+
+class ImageInLine(SortableInlineAdminMixin, admin.TabularInline):
+    model = Image
+    readonly_fields = ['get_preview_image']
+    list_display = ['get_preview_image', 'order']
+    extra = 0
+
+    def get_preview_image(self, obj):
+        if not obj.image:
+            return '-'
+        return mark_safe(f'<img src="{obj.image.url}" height="{130}" />')
+
+    get_preview_image.short_description = 'Image Preview'
+
+
+@admin.register(Place)
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
+    inlines = [ImageInLine]
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description_short', 'description_long', ('longitude', 'latitude'))
+        }),
+    )
+
+
+admin.site.register(Image)
